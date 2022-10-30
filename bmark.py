b5213e3a8ae5aee1e81138052bfeef84a0617836
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2022-10-30 13:47:32 +0100

    Moved all functions and variables from Bmark class to module level.

--- bmark.py
+++ bmark.py
@@ -2,231 +2,205 @@ import gc
 import time
 from typing import Callable, Dict, List, Optional
 
+"""
+A collection of benchmark functions useful in data science applications
+"""
+_last_time: Optional[float] = None
+_time_dict: Dict[str, List[float]] = {}
+_accumulate = False
 
-class Bmark:
+
+def _disabled_garbage(func: Callable):
     """
-    A collection of benchmark functions useful in data science applications
+    Disables garbage collecting when measuring a given function.
     """
 
-    _last_time: Optional[float] = None
-    _time_dict: Dict[str, List[float]] = {}
-    _accumulate = False
+    def wrapper(*args, **kwargs):
+        gc_old = gc.isenabled()
+        gc.disable()
+        result = func(*args, **kwargs)
+        if gc_old:
+            gc.enable()
+        return result
 
-    @staticmethod
-    def _disabled_garbage(func: Callable):
-        """
-        Disables garbage collecting when measuring a given function.
-        """
+    return wrapper
 
+
+def _accumulate_to_dict(func_id: str, measurement: float):
+    """
+    Appends given measurement to a list under the func_id key.
+    """
+    if func_id not in _time_dict.keys():
+        _time_dict[func_id] = []
+    _time_dict[func_id].append(measurement)
+
+
+def _get_func_times(func_id: str) -> Optional[List[float]]:
+    """
+    Returns a list of all measured function times if they exist, None
+    otherwise.
+    """
+    if func_id not in _time_dict:
+        return None
+    return _time_dict[func_id]
+
+
+def _get_time_sum_func(func_id: str) -> Optional[float]:
+    """
+    Returns a sum of all measurements of a given function if they exist,
+    None otherwise.
+    """
+    if func_id not in _time_dict:
+        return None
+    return sum(_time_dict[func_id])
+
+
+def measure_time(*func_ids: str) -> Callable:
+    """
+    Time measurement decorator.
+    Whenever the decorated function (or method) is called the decorator
+    measures the time is takes to finish it. This decorator temporarily
+    disables garbage collecting in order to get correct measurements.
+    This decorator can have specified function ids as arguments. This will
+    make the class associate all the measurements with the given ids
+    (provided ``enable_accumulating()`` has been called
+    beforehand).
+    """
+
+    def decorator(func: Callable) -> Callable:
+        @_disabled_garbage
         def wrapper(*args, **kwargs):
-
-            gc_old = gc.isenabled()
-            gc.disable()
-
+            start = time.process_time()
             result = func(*args, **kwargs)
-
-            if gc_old:
-                gc.enable()
-
+            measurement = time.process_time() - start
+            if _accumulate:
+                for func_id in func_ids:
+                    _accumulate_to_dict(func_id, measurement)
+            global _last_time
+            _last_time = measurement
             return result
 
         return wrapper
 
-    @staticmethod
-    def _accumulate_to_dict(func_id: str, measurement: float):
-        """
-        Appends given measurement to a list under the func_id key.
-        """
-        if func_id not in Bmark._time_dict.keys():
-            Bmark._time_dict[func_id] = []
+    return decorator
 
-        Bmark._time_dict[func_id].append(measurement)
 
-    @staticmethod
-    def _get_func_times(func_id: str) -> Optional[List[float]]:
-        """
-        Returns a list of all measured function times if they exist, None
-        otherwise.
-        """
-        if func_id not in Bmark._time_dict:
-            return None
+def get_measured_time() -> Optional[float]:
+    """
+    Returns the last measurement (``None`` if there was none or if
+    ``reset_measured_time()`` has been invoked).
+    """
+    return _last_time
 
-        return Bmark._time_dict[func_id]
 
-    @staticmethod
-    def _get_time_sum_func(func_id: str) -> Optional[float]:
-        """
-        Returns a sum of all measurements of a given function if they exist,
-        None otherwise.
-        """
-        if func_id not in Bmark._time_dict:
-            return None
-
-        return sum(Bmark._time_dict[func_id])
-
-    @staticmethod
-    def measure_time(*func_ids: str) -> Callable:
-        """
-        Time measurement decorator.
-
-        Whenever the decorated function (or method) is called the decorator
-        measures the time is takes to finish it. This decorator temporarily
-        disables garbage collecting in order to get correct measurements.
-
-        This decorator can have specified function ids as arguments. This will
-        make the class associate all the measurements with the given ids
-        (provided ``Bmark.enable_accumulating()`` has been called
-        beforehand).
-        """
-
-        def decorator(func: Callable) -> Callable:
-            @Bmark._disabled_garbage
-            def wrapper(*args, **kwargs):
-
-                start = time.process_time()
-                result = func(*args, **kwargs)
-                measurement = time.process_time() - start
-
-                if Bmark._accumulate:
-                    for func_id in func_ids:
-                        Bmark._accumulate_to_dict(func_id, measurement)
-                Bmark._last_time = measurement
-
-                return result
-
-            return wrapper
-
-        return decorator
-
-    @staticmethod
-    def get_measured_time() -> Optional[float]:
-        """
-        Returns the last measurement (``None`` if there was none or if
-        ``Bmark.reset_measured_time()`` has been invoked).
-        """
-        return Bmark._last_time
-
-    @staticmethod
-    def get_times_funcs(
-        func_id: str, *func_ids: str
-    ) -> None | List[float] | Dict[str, None | List[float]]:
-        """
-        If a single function id is given, returns a list of all measurements of
-        the function if exists, ``None`` otherwise. If instead multiple ids are
-        given, returns a dictionary with function ids as keys and similar lists
-        as values (with all measurements of each function accordingly, ``None``
-        if there are no such measurements).
-        """
-
-        if len(func_ids) == 0:
-            return Bmark._get_func_times(func_id)
-
-        else:
-            result: Dict[str, None | List[float]] = {}
-            result[func_id] = Bmark._get_func_times(func_id)
-
-            for id in func_ids:
-                result[id] = Bmark._get_func_times(id)
-
-            return result
-
-    @staticmethod
-    def get_last_func_time(func_id: str) -> Optional[float]:
-        """
-        Returns the last measurement performed on a function (or ``None`` if
-        there are no such measurements).
-        """
-
-        if func_id not in Bmark._time_dict:
-            return None
-
-        return Bmark._time_dict[func_id][-1]
-
-    @staticmethod
-    def get_time_sum_funcs(func_id: str, *func_ids: str) -> Optional[float]:
-        """
-        Returns a sum of all measurements of all functions given as parameters
-        (or ``None`` if none of the functions has any measurements).
-        """
-
-        Bmark._remaining_ids = list(Bmark._time_dict.keys())
-        result = Bmark._get_time_sum_func(func_id)
-
-        try:
-            Bmark._remaining_ids.remove(func_id)
-        except ValueError:
-            result = None
-
+def get_times_funcs(
+    func_id: str, *func_ids: str
+) -> None | List[float] | Dict[str, None | List[float]]:
+    """
+    If a single function id is given, returns a list of all measurements of
+    the function if exists, ``None`` otherwise. If instead multiple ids are
+    given, returns a dictionary with function ids as keys and similar lists
+    as values (with all measurements of each function accordingly, ``None``
+    if there are no such measurements).
+    """
+    if len(func_ids) == 0:
+        return _get_func_times(func_id)
+    else:
+        result: Dict[str, None | List[float]] = {}
+        result[func_id] = _get_func_times(func_id)
         for id in func_ids:
-
-            func_time = Bmark._get_time_sum_func(id)
-
-            try:
-                Bmark._remaining_ids.remove(id)
-            except ValueError:
-                func_time = None
-
-            if func_time is not None:
-                if result is None:
-                    result = 0
-                result += func_time
-
-        del Bmark._remaining_ids
+            result[id] = _get_func_times(id)
         return result
 
-    @staticmethod
-    def get_time_sum_all_funcs() -> Optional[float]:
-        """
-        Returns a sum of all measurements of all functions (or ``None`` if none
-        of the functions has any measurements).
-        """
 
-        if len(Bmark._time_dict) == 0:
-            return None
+def get_last_func_time(func_id: str) -> Optional[float]:
+    """
+    Returns the last measurement performed on a function (or ``None`` if
+    there are no such measurements).
+    """
+    if func_id not in _time_dict:
+        return None
+    return _time_dict[func_id][-1]
 
-        return Bmark.get_time_sum_funcs(*Bmark._time_dict.keys())
 
-    @staticmethod
-    def enable_accumulating():
-        """Enables storing all performed measurements to an inner dict."""
-        Bmark._accumulate = True
+def get_time_sum_funcs(func_id: str, *func_ids: str) -> Optional[float]:
+    """
+    Returns a sum of all measurements of all functions given as parameters
+    (or ``None`` if none of the functions has any measurements).
+    """
+    _remaining_ids = list(_time_dict.keys())
+    result = _get_time_sum_func(func_id)
+    try:
+        _remaining_ids.remove(func_id)
+    except ValueError:
+        result = None
+    for id in func_ids:
+        func_time = _get_time_sum_func(id)
+        try:
+            _remaining_ids.remove(id)
+        except ValueError:
+            func_time = None
+        if func_time is not None:
+            if result is None:
+                result = 0
+            result += func_time
+    del _remaining_ids
+    return result
 
-    @staticmethod
-    def disable_accumulating():
-        """Disables storing all performed measurements to an inner dict."""
-        Bmark._accumulate = False
 
-    @staticmethod
-    def reset_measured_time():
-        """
-        Resets last measured time. After this call, ``get_measured_time``
-        will return None.
-        """
-        Bmark._last_time = None
+def get_time_sum_all_funcs() -> Optional[float]:
+    """
+    Returns a sum of all measurements of all functions (or ``None`` if none
+    of the functions has any measurements).
+    """
+    if len(_time_dict) == 0:
+        return None
+    return get_time_sum_funcs(*_time_dict.keys())
 
-    @staticmethod
-    def _reset_func_times(func_id: str):
-        """Resets all stored measurements of a given function."""
-        if func_id in Bmark._time_dict.keys():
-            Bmark._time_dict.pop(func_id)
 
-    @staticmethod
-    def reset_func_times(func_id: str, *func_ids: str):
-        """
-        Resets all stored measurements of all functions given as
-        parameters.
-        """
-        Bmark._reset_func_times(func_id)
-        for id in func_ids:
-            Bmark._reset_func_times(id)
+def enable_accumulating():
+    """Enables storing all performed measurements to an inner dict."""
+    global _accumulate
+    _accumulate = True
 
-    @staticmethod
-    def reset_all_func_times():
-        """Resets all stored measurements of all functions."""
-        Bmark._time_dict.clear()
 
-    @staticmethod
-    def reset_to_default():
-        Bmark.reset_all_func_times()
-        Bmark.reset_measured_time()
-        Bmark.disable_accumulating()
+def disable_accumulating():
+    """Disables storing all performed measurements to an inner dict."""
+    global _accumulate
+    _accumulate = False
+
+
+def reset_measured_time():
+    """
+    Resets last measured time. After this call, ``get_measured_time``
+    will return None.
+    """
+    global _last_time
+    _last_time = None
+
+
+def _reset_func_times(func_id: str):
+    """Resets all stored measurements of a given function."""
+    if func_id in _time_dict.keys():
+        _time_dict.pop(func_id)
+
+
+def reset_func_times(func_id: str, *func_ids: str):
+    """
+    Resets all stored measurements of all functions given as
+    parameters.
+    """
+    _reset_func_times(func_id)
+    for id in func_ids:
+        _reset_func_times(id)
+
+
+def reset_all_func_times():
+    """Resets all stored measurements of all functions."""
+    _time_dict.clear()
+
+
+def reset_to_default():
+    reset_all_func_times()
+    reset_measured_time()
+    disable_accumulating()
--- tests/test_bmark.py
+++ tests/test_bmark.py
@@ -1,161 +1,161 @@
+import bmark
 import pytest
-from bmark import Bmark
 
 
-@Bmark.measure_time()
+@bmark.measure_time()
 def func():
     pass
 
 
-@Bmark.measure_time("func1")
+@bmark.measure_time("func1")
 def func1():
     pass
 
 
-@Bmark.measure_time("func2")
+@bmark.measure_time("func2")
 def func2():
     pass
 
 
-@Bmark.measure_time("func1", "func2")
+@bmark.measure_time("func1", "func2")
 def func12():
     pass
 
 
 @pytest.fixture(autouse=True)
 def reset_bmark():
-    Bmark.reset_to_default()
+    bmark.reset_to_default()
 
 
 class Cls:
     def __init__(self):
         pass
 
-    @Bmark.measure_time("class")
+    @bmark.measure_time("class")
     def method(self):
         pass
 
 
 def test_defaults():
-    assert Bmark.get_measured_time() is None
-    assert Bmark.get_times_funcs("func1") is None
+    assert bmark.get_measured_time() is None
+    assert bmark.get_times_funcs("func1") is None
 
 
 def test_measure_time():
     func1()
-    assert Bmark.get_measured_time() is not None
-    assert Bmark.get_times_funcs("func1") is None
+    assert bmark.get_measured_time() is not None
+    assert bmark.get_times_funcs("func1") is None
 
 
 def test_measure_time_accumulate():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
-    assert Bmark.get_measured_time() is not None
-    assert Bmark.get_times_funcs("func1") is not None
-    assert len(Bmark.get_times_funcs("func1")) == 1  # type: ignore
-    assert Bmark.get_times_funcs("func2") is None
+    assert bmark.get_measured_time() is not None
+    assert bmark.get_times_funcs("func1") is not None
+    assert len(bmark.get_times_funcs("func1")) == 1  # type: ignore
+    assert bmark.get_times_funcs("func2") is None
 
 
 def test_measure_time_acc_two_funcs():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
     func2()
-    assert Bmark.get_measured_time() is not None
-    assert Bmark.get_times_funcs("func1") is not None
-    assert Bmark.get_times_funcs("func2") is not None
-    assert len(Bmark.get_times_funcs("func1")) == 1  # type: ignore
-    assert len(Bmark.get_times_funcs("func2")) == 1  # type: ignore
+    assert bmark.get_measured_time() is not None
+    assert bmark.get_times_funcs("func1") is not None
+    assert bmark.get_times_funcs("func2") is not None
+    assert len(bmark.get_times_funcs("func1")) == 1  # type: ignore
+    assert len(bmark.get_times_funcs("func2")) == 1  # type: ignore
 
 
 def test_measure_time_acc_two_funcs_sum():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
     func2()
-    time1 = Bmark.get_time_sum_funcs("func1")
-    time2 = Bmark.get_time_sum_funcs("func2")
-    time_sum = Bmark.get_time_sum_all_funcs()
+    time1 = bmark.get_time_sum_funcs("func1")
+    time2 = bmark.get_time_sum_funcs("func2")
+    time_sum = bmark.get_time_sum_all_funcs()
     assert time_sum == (time1 + time2)  # type: ignore
 
 
 def test_measure_time_acc_no_id():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func()
-    assert Bmark.get_time_sum_all_funcs() is None
+    assert bmark.get_time_sum_all_funcs() is None
 
 
 def test_time_reset():
     func1()
-    Bmark.reset_measured_time()
-    assert Bmark.get_measured_time() is None
+    bmark.reset_measured_time()
+    assert bmark.get_measured_time() is None
 
 
 def test_deleting_func_times():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
     func2()
-    Bmark.reset_func_times("func1")
-    assert Bmark.get_times_funcs("func1") is None
-    assert Bmark.get_times_funcs("func2") is not None
-    assert Bmark.get_time_sum_funcs("func1") is None
-    assert Bmark.get_time_sum_all_funcs() is not None
+    bmark.reset_func_times("func1")
+    assert bmark.get_times_funcs("func1") is None
+    assert bmark.get_times_funcs("func2") is not None
+    assert bmark.get_time_sum_funcs("func1") is None
+    assert bmark.get_time_sum_all_funcs() is not None
 
 
 def test_deleting_all_func_times():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
     func2()
-    Bmark.reset_all_func_times()
-    assert Bmark.get_times_funcs("func1") is None
-    assert Bmark.get_times_funcs("func2") is None
-    assert Bmark.get_time_sum_funcs("func1") is None
-    assert Bmark.get_time_sum_funcs("func2") is None
-    assert Bmark.get_time_sum_all_funcs() is None
+    bmark.reset_all_func_times()
+    assert bmark.get_times_funcs("func1") is None
+    assert bmark.get_times_funcs("func2") is None
+    assert bmark.get_time_sum_funcs("func1") is None
+    assert bmark.get_time_sum_funcs("func2") is None
+    assert bmark.get_time_sum_all_funcs() is None
 
 
 def test_method():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     cls = Cls()
     cls.method()
-    assert Bmark.get_times_funcs("class") is not None
-    assert Bmark.get_time_sum_funcs("class") is not None
-    assert Bmark.get_time_sum_all_funcs() is not None
+    assert bmark.get_times_funcs("class") is not None
+    assert bmark.get_time_sum_funcs("class") is not None
+    assert bmark.get_time_sum_all_funcs() is not None
 
 
 def test_measure_time_acc_two_ids():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func12()
-    assert Bmark.get_times_funcs("func1") is not None
-    assert Bmark.get_times_funcs("func2") is not None
-    assert Bmark.get_time_sum_funcs("func1") is not None
-    assert Bmark.get_time_sum_funcs("func2") is not None
-    assert Bmark.get_time_sum_all_funcs() is not None
+    assert bmark.get_times_funcs("func1") is not None
+    assert bmark.get_times_funcs("func2") is not None
+    assert bmark.get_time_sum_funcs("func1") is not None
+    assert bmark.get_time_sum_funcs("func2") is not None
+    assert bmark.get_time_sum_all_funcs() is not None
 
 
 def test_get_multiple_times():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
     func2()
-    dict = Bmark.get_times_funcs("func1", "func2")
+    dict = bmark.get_times_funcs("func1", "func2")
     assert "func1" in dict.keys()  # type: ignore
     assert "func2" in dict.keys()  # type: ignore
 
 
 def test_get_time_sum_funcs_variadic():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
     func2()
     time1, time2, time12 = (
-        Bmark.get_time_sum_funcs("func1"),
-        Bmark.get_time_sum_funcs("func2"),
-        Bmark.get_time_sum_funcs("func1", "func2"),
+        bmark.get_time_sum_funcs("func1"),
+        bmark.get_time_sum_funcs("func2"),
+        bmark.get_time_sum_funcs("func1", "func2"),
     )
     assert time12 == (time1 + time2)  # type: ignore
 
 
 def test_get_time_sum_funcs_variadic_repeats():
-    Bmark.enable_accumulating()
+    bmark.enable_accumulating()
     func1()
     func2()
-    time1 = Bmark.get_time_sum_funcs("func1")
-    time11 = Bmark.get_time_sum_funcs("func1", "func1")
+    time1 = bmark.get_time_sum_funcs("func1")
+    time11 = bmark.get_time_sum_funcs("func1", "func1")
     assert time1 == time11
